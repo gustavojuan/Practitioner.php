21458696a75d71c3f181810a04cc0b190056c4b5
Gustavo Juan <gustavojuan@MBP-de-Gustavo.home>
2016-09-04 11:50:13 +0200

    PHP & HTML => Echo alias inside HTML

--- www/index.php
+++ www/index.php
@@ -1,12 +1,21 @@
-<?php
-$greeting = "Hello";
-$name = "John Doe";
-
-echo 'Hello, $name';
-echo "<br>";
-echo "Hello, $name";
-echo "<br>";
-echo "Hello, ".$name;
-echo "<br>";
-echo "{$greeting} , {$name}";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        h1 {
+            background-color: ;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1><?= "Hello, ".$_GET['name']  ?></h1>
+</body>
+</html>
 
